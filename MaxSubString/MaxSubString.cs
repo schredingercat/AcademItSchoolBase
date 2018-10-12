8bc9e9a211f5f0573ee4eb46561f9c54e3cf1e1b
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-10-12 14:20:26 +0700

    Добавлен проект MaxSubString

---
+++ MaxSubString/MaxSubString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MaxSubstring
+{
+    class MaxSubstring
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку для поиска");
+            var output = GetMaxSubstring(Console.ReadLine());
+
+            Console.WriteLine($"Максимум одинаковых символов подряд: {output}");
+            Console.ReadLine();
+        }
+
+        private static double GetMaxSubstring(string input)
+        {
+            if (input.Length <= 1)
+            {
+                return input.Length;
+            }
+
+            var lowercaseString = input.ToLower();
+            var substringLength = 1;
+            var maxSubstringLength = 0;
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                substringLength = (lowercaseString[i] == lowercaseString[i - 1]) ? ++substringLength : 1;
+
+                if (substringLength > maxSubstringLength)
+                {
+                    maxSubstringLength = substringLength;
+                }
+            }
+
+            return maxSubstringLength;
+        }
+    }
+}
