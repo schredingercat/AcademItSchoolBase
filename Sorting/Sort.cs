0e1c1cf2954d5919442aa1ee5a1622d1adeb417e
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-10-29 14:43:37 +0700

    Реализована сортировка выбором

---
+++ Sorting/Sort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sort
+    {
+
+        public static void SelectSort(int[] input)
+        {
+            var length = input.Length;
+            for (int i = 0; i < length; i++)
+            {
+                var minIndex = FindMinIndex(input, i);
+                var temp = input[minIndex];
+                input[minIndex] = input[i];
+                input[i] = temp;
+            }
+        }
+
+        public static int FindMinIndex(int[] input, int startIndex)
+        {
+            var minIndex = startIndex;
+            var min = input[startIndex];
+            var length = input.Length;
+
+            for (int i = startIndex; i < length; i++)
+            {
+                if (input[i] < min)
+                {
+                    min = input[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+    }
+}
---
+++ Sorting/Sorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Sorting
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[10];
+
+            var random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+
+            Console.WriteLine($"Начальный массив: {string.Join(", ", array)}");
+
+            Sort.SelectSort(array);
+            Console.WriteLine($"Сортировка выбором: {string.Join(", ", array)}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
