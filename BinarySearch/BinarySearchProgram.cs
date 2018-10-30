f343a3d2cd082912e4619567bf042eb47b12be8b
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-10-30 16:36:09 +0700

    Добавлен проект Бинарный поиск

---
+++ BinarySearch/BinarySearchProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class BinarySearchProgram
+    {
+        static void Main()
+        {
+            var array = new[] { 4, 8, 15, 16, 23, 42 };
+            var x = 16;
+
+            Console.WriteLine($"Массив: {string.Join(", ", array)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Бинарный поиск {x} с рекурсией:  {Search.BinarySearch(array, 0, array.Length - 1, x)}");
+            Console.WriteLine($"Бинарный поиск {x} без рекурсии: {Search.BinarySearchWithoutRecurse(array, x)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Бинарный поиск {19} с рекурсией:  {Search.BinarySearch(array, 0, array.Length - 1, 19)}");
+            Console.WriteLine($"Бинарный поиск {108} без рекурсии: {Search.BinarySearchWithoutRecurse(array, 108)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BinarySearch/Search.cs
@@ -0,0 +1,64 @@
+﻿namespace BinarySearch
+{
+    class Search
+    {
+        public static int BinarySearch(int[] array, int left, int right, int x)
+        {
+            if (left > right)
+            {
+                return -1;
+            }
+
+            var middle = (right + left) / 2;
+
+            if (array[middle] == x)
+            {
+                return middle;
+            }
+            if (array[middle] > x)
+            {
+                right = middle - 1;
+            }
+            else if (array[middle] < x)
+            {
+                left = middle + 1;
+            }
+
+            if (left > right)
+            {
+                return -1;
+            }
+
+            return BinarySearch(array, left, right, x);
+        }
+
+        public static int BinarySearchWithoutRecurse(int[] array, int x)
+        {
+            var left = 0;
+            var right = array.Length - 1;
+            if (left > right)
+            {
+                return -1;
+            }
+
+            while (left <= right)
+            {
+                var middle = (right + left) / 2;
+
+                if (array[middle] == x)
+                {
+                    return middle;
+                }
+                if (array[middle] > x)
+                {
+                    right = middle - 1;
+                }
+                else if (array[middle] < x)
+                {
+                    left = middle + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
