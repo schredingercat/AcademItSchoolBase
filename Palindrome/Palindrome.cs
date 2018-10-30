7e71961290ebe588390e3e3ec0b7094dff72aef6
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-10-30 18:20:33 +0700

    Поиск начинается с последней найденной буквы, строка проверяется только до середины

--- Palindrome/Palindrome.cs
+++ Palindrome/Palindrome.cs
@@ -16,10 +16,15 @@ namespace Palindrome
 
         public static bool IsPalindrome(string input)
         {
+            var length = input.Length;
             var inputLowered = input.ToLower();
-            for (int i = 0; i < input.Length; i++)
+            var lastIndexForward = 0;
+            var lastIndexBackward = length;
+
+            for (int i = 0; i < length / 2; i++)
             {
-                if (GetLetterByIndexForward(inputLowered, i) != GetLetterByIndexBackward(inputLowered, i))
+                if (GetLetterByIndexForward(inputLowered, i, lastIndexForward, out lastIndexForward)
+                    != GetLetterByIndexBackward(inputLowered, i, lastIndexBackward, out lastIndexBackward))
                 {
                     return false;
                 }
@@ -27,16 +32,17 @@ namespace Palindrome
             return true;
         }
 
-        public static char GetLetterByIndexForward(string input, int index)
+        public static char GetLetterByIndexForward(string input, int index, int lastIndex, out int outLastIndex)
         {
+            outLastIndex = lastIndex;
             if (index < 0 || index > input.Length)
             {
                 return char.MinValue;
             }
 
-            var letterNumber = 0;
+            var letterNumber = index;
 
-            for (int i = 0; i < input.Length; i++)
+            for (int i = lastIndex; i < input.Length; i++)
             {
                 if (!char.IsLetter(input[i]))
                 {
@@ -45,6 +51,7 @@ namespace Palindrome
 
                 if (letterNumber == index)
                 {
+                    outLastIndex = i + 1;
                     return input[i];
                 }
 
@@ -54,16 +61,17 @@ namespace Palindrome
             return char.MinValue;
         }
 
-        public static char GetLetterByIndexBackward(string input, int index)
+        public static char GetLetterByIndexBackward(string input, int index, int lastIndex, out int outLastIndex)
         {
+            outLastIndex = lastIndex;
             if (index < 0 || index > input.Length)
             {
                 return char.MinValue;
             }
 
-            var letterNumber = 0;
+            var letterNumber = index;
 
-            for (int i = input.Length - 1; i >= 0; i--)
+            for (int i = lastIndex - 1; i >= 0; i--)
             {
                 if (!char.IsLetter(input[i]))
                 {
@@ -72,6 +80,7 @@ namespace Palindrome
 
                 if (letterNumber == index)
                 {
+                    outLastIndex = i;
                     return input[i];
                 }
 
