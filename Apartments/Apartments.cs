fa728da0be4a47becadd83067183af49ab293e90
Ilya Bogomolov <bogomolov_comfortlab@bitbucket.org>
2018-11-01 16:10:42 +0700

    Переименование переменных

--- Apartments/Apartments.cs
+++ Apartments/Apartments.cs
@@ -5,58 +5,59 @@ namespace Apartments
 {
     class Apartments
     {
-        const int ApartmentsInFloor = 4;
-
-        static void Main()
-        {
-            Console.WriteLine("Введите количество этажей");
-            var floorInputIsValid = int.TryParse(Console.ReadLine(), out int floorNumber);
-
-            Console.WriteLine("Введите количество подъездов");
-            var sectionInputIsValid = int.TryParse(Console.ReadLine(), out int sectionNumber);
-
-            Console.WriteLine("Введите номер квартиры");
-            var apartmentInputIsValid = int.TryParse(Console.ReadLine(), out int apartmentId);
-
-            if (!floorInputIsValid || !sectionInputIsValid || !apartmentInputIsValid || floorNumber <= 0 || sectionNumber <= 0 || apartmentId <= 0)
-            {
-                Console.WriteLine("Введены некорректные данные");
-                Console.ReadLine();
-                return;
-            }
-
-            if (apartmentId > ApartmentsInFloor * floorNumber * sectionNumber)
-            {
-                Console.WriteLine("В доме нет такой квартиры");
-                Console.ReadLine();
-                return;
-            }
-
-            Console.WriteLine($"Квартира в {FindSection(floorNumber, apartmentId)} подъезде на {FindFloor(floorNumber, apartmentId)} этаже, {FindPosition(apartmentId)}");
-            Console.ReadLine();
-        }
-
-        private static int FindSection(int floorNumber, int apartmentId)
-        {
-            return (apartmentId - 1) / (floorNumber * ApartmentsInFloor) + 1;
-        }
-
-        private static int FindFloor(int floorNumber, int apartmentId)
-        {
-            return (apartmentId - 1) % (floorNumber * ApartmentsInFloor) / ApartmentsInFloor + 1;
-        }
-
-        private static string FindPosition(int apartmentId)
-        {
-            return ApartmentPositions[(apartmentId - 1) % ApartmentsInFloor + 1];
-        }
-
-        private static readonly Dictionary<int, string> ApartmentPositions = new Dictionary<int, string>()
+        private static readonly Dictionary<int, string> ApartmentPositions = new Dictionary<int, string>
         {
             [1] = "ближняя слева",
             [2] = "дальняя слева",
             [3] = "дальняя справа",
             [4] = "ближняя справа"
         };
+
+        const int ApartmentsInFloorCount = 4;
+
+        static void Main()
+        {
+            Console.WriteLine("Введите количество этажей");
+            var floorsInputIsValid = int.TryParse(Console.ReadLine(), out int floorsCount);
+
+            Console.WriteLine("Введите количество подъездов");
+            var sectionsInputIsValid = int.TryParse(Console.ReadLine(), out int sectionsCount);
+
+            Console.WriteLine("Введите номер квартиры");
+            var apartmentInputIsValid = int.TryParse(Console.ReadLine(), out int apartmentNumber);
+
+            if (!floorsInputIsValid || !sectionsInputIsValid || !apartmentInputIsValid || floorsCount <= 0 || sectionsCount <= 0 || apartmentNumber <= 0)
+            {
+                Console.WriteLine("Введены некорректные данные");
+                Console.ReadLine();
+                return;
+            }
+
+            if (apartmentNumber > ApartmentsInFloorCount * floorsCount * sectionsCount)
+            {
+                Console.WriteLine("В доме нет такой квартиры");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine($"Квартира в {FindSection(floorsCount, apartmentNumber)} подъезде " +
+                              $"на {FindFloor(floorsCount, apartmentNumber)} этаже, {FindPosition(apartmentNumber)}");
+            Console.ReadLine();
+        }
+
+        private static int FindSection(int floorsCount, int apartmentNumber)
+        {
+            return (apartmentNumber - 1) / (floorsCount * ApartmentsInFloorCount) + 1;
+        }
+
+        private static int FindFloor(int floorsCount, int apartmentNumber)
+        {
+            return (apartmentNumber - 1) % (floorsCount * ApartmentsInFloorCount) / ApartmentsInFloorCount + 1;
+        }
+
+        private static string FindPosition(int apartmentNumber)
+        {
+            return ApartmentPositions[(apartmentNumber - 1) % ApartmentsInFloorCount + 1];
+        }
     }
 }
