5f8ec0a9cb1088e46a2ac399f59b0b550ea208f3
Богомолов Илья <schredinger@mail.ru>
2018-11-05 13:11:57 +0700

    Добавлен проект ImageTest

---
+++ ImageTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace ImageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxRgb = 255;
+            Bitmap image = new Bitmap("image.jpg");
+            
+            for (int y = 0; y < image.Height; ++y)
+            {
+                for (int x = 0; x < image.Width; ++x)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(maxRgb - pixel.R, maxRgb - pixel.G, maxRgb - pixel.B);
+                    
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+            image.Save("out.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
